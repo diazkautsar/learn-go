c1b327d4712507f40ea5913a99d8ac350a5bbae6
Diaz <diazkautsar77@gmail.com>
2022-01-02 15:17:35 +0700

    modifiy module hello for get greetings module

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+
+	"example.com/greetings"
+
+	"log"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	fmt.Println(quote.Go())
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	names := []string{"Djalal", "Dudung", "Dalmajid"} // slice name
+
+	// message, err := greetings.Hello("") // dont have value
+	// message, err := greetings.Hello("Djalal") // have value
+	message, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
 }
