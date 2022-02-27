24e20187302baebc8d74b4f721d30ba33606b09e
Diaz <diazkautsar77@gmail.com>
2022-02-27 09:32:14 +0700

    constant

---
+++ golang-dasar/constant.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	const (
+		firstName string = "Djalal"
+		lastName         = "Kurnia"
+	)
+}
+
+// constant cant change value
+// constant must have value
+// constant can set multiple declaration
+// constant jika tidak digunakan dia tidak akan error
