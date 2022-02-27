82b19adfd2cddd1705fa0e72e651427dc05c752d
Diaz <diazkautsar77@gmail.com>
2022-02-27 09:33:35 +0700

    string

---
+++ golang-dasar/string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Diaz")
+	fmt.Println("Djalal Kurnia")
+
+	fmt.Println(len("Diaz"))
+	fmt.Println("Djalal Kurnia"[0]) // 68 => refresentasi byte
+	fmt.Println("Djalal Kurnia"[1]) // 106 => refresntasi byte
+}
